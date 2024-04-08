e6a36c9a171a170c64f3eed6154194f80f15242d
CTimmins1 <116078899+CTimmins1@users.noreply.github.com>
2024-04-08 21:51:41 +0100

    12:18 08/04/2024

--- AndroidStudio/CloverCycle/app/src/main/java/com/example/clovercycle/DatabaseSqlite.java
+++ AndroidStudio/CloverCycle/app/src/main/java/com/example/clovercycle/DatabaseSqlite.java
@@ -1,8 +1,13 @@
 package com.example.clovercycle;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
+import android.content.ContentValues;
+
+import java.util.ArrayList;
+
 
 public class DatabaseSqlite extends SQLiteOpenHelper {
 
@@ -25,13 +30,12 @@ public class DatabaseSqlite extends SQLiteOpenHelper {
     private static final String KEY_ID = "id";
     private static final String KEY_USER_NAME = "user_name";
     private static final String KEY_PASSWORD = "password";
-    private static final String KEY_ADDRESS = "address";
+    public static final String KEY_ADDRESS = "address";
 
     private static final String KEY_EMAIL ="email";
     private static final String KEY_AMOUNT = "amount";
 
-    private static final String KEY_NAME ="name";
-
+    public static final String KEY_NAME ="name";
 
 
 
@@ -62,6 +66,8 @@ public class DatabaseSqlite extends SQLiteOpenHelper {
     public DatabaseSqlite(Context context) {
         super(context, DB_NAME, null, DATABASE_VERSION);
     }
+
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         // creating required tables
@@ -79,5 +85,22 @@ public class DatabaseSqlite extends SQLiteOpenHelper {
         // create new tables
         onCreate(db);
     }
+    public ArrayList<String> getJobs() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<String> jobsList = new ArrayList<>();
+        Cursor cursor = db.rawQuery("SELECT " + KEY_NAME + " FROM " + TABLE_JOBS, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                String jobName = cursor.getString(cursor.getColumnIndex(KEY_NAME));
+                jobsList.add(jobName);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return jobsList;
+    }
+
 
 }
--- AndroidStudio/CloverCycle/app/src/main/java/com/example/clovercycle/UserJobActivity.java
+++ AndroidStudio/CloverCycle/app/src/main/java/com/example/clovercycle/UserJobActivity.java
@@ -1,11 +1,17 @@
 package com.example.clovercycle;
 
+import static com.example.clovercycle.DatabaseSqlite.KEY_NAME;
+
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,35 +22,78 @@ public class UserJobActivity extends AppCompatActivity {
     EditText amountInput;
     Button jobBtn;
     Button menuBtn;
+    String name, address, amount;
+    int result;
     DatabaseSqlite dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_job);
+        //initiate the database
         dbHelper = new DatabaseSqlite(this);
         //find nameTxt, addressTxt, and Button
         nameTxt = findViewById(R.id.nameTxt);
         addressTxt = findViewById(R.id.addressTxt);
         amountInput = findViewById(R.id.amountInput);
         jobBtn = findViewById(R.id.jobBtn);
-
-
         //create event listener
         menuBtn = (Button) findViewById(R.id.menuBtn);
         menuBtn.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View v) {
                 logoutActivity();
             }
         });
-    }
-            public void logoutActivity() {
-                Intent intent1 = new Intent(this, MainActivity.class);
-                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent1);
-                finish();
+
+        jobBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve values from EditText fields
+                name = nameTxt.getText().toString();
+                address = addressTxt.getText().toString();
+                amount = amountInput.getText().toString();
+
+                // Call postJob method to insert data into database
+                postJob();
             }
+        });
+
+    }
+
+    private void postJob() {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues jobs = new ContentValues();
+
+        // inset data as we wish
+        jobs.put("name", name);
+        jobs.put("address", address);
+        jobs.put("amount", amount);
+
+        db.insert("users", null, jobs);
+
+        db.close();
+        //if result is not -1, this indicates a successful inserstion and will display toast message
+        if (result != -1) {
+            //data insertion was successful
+            //toast is a android studio utility that allows to show messages
+            showToast("Job posted successfully");
+        } else {
+            //data insertion failed
+            showToast("Failed to post job");
         }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void logoutActivity() {
+        Intent intent1 = new Intent(this, MainActivity.class);
+        intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent1);
+        finish();
+    }
+
+
+}
